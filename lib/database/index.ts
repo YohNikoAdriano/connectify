de7977ace7f89688446ebf0e1d360059b684685a
YohNikoAdriano <yoh.nikoadriano@gmail.com>
2025-02-07 02:52:20 +0700

    debug 2

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,16 +1,32 @@
-import mongoose from 'mongoose'
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const MONGODB_URI = process.env.MONGODB_URI
+// Store the cached connection
+let cached = (global as any).mongo || { conn: null, promise: null };
 
-let cached = (global as any).mongoose || {conn: null, promise: null}
+const MONGODB_URI = "mongodb+srv://yohnikoadriano:iE6ABagbGDWWm1zr@cluster0.i5dsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 export const connectToDB = async () => {
-    if(cached.conn) return cached.conn
-    if(!MONGODB_URI) throw new Error('MongoDB URI is missing')
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'connectify',
-        bufferCommands: false
+    if (cached.conn) return cached.conn; // Return the cached connection if it exists
+
+    if (!MONGODB_URI) throw new Error('MongoDB URI is missing');
+
+    // If no cached connection, start a new one
+    cached.promise = cached.promise || MongoClient.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
     })
-    cached.conn = await cached.promise
-    return cached.conn
-}
+    .then((client: { db: (arg0: string) => any; }) => {
+        cached.conn = client.db('connectify'); // Select the 'connectify' database
+        return cached.conn;
+    });
+
+    // Wait for the connection to resolve
+    await cached.promise;
+    return cached.conn;
+};
