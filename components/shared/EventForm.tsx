01f7e2b093e2cbdf6ea899c7a034f6db4de8b2dc
YohNikoAdriano <yoh.nikoadriano@gmail.com>
2025-02-12 04:53:55 +0700

    update form - untul date picker

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -17,6 +17,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 // constant
 import { eventDefaultValues } from "@/constant";
@@ -24,6 +25,9 @@ import { eventDefaultValues } from "@/constant";
 // lib
 import { eventFormSchema } from "@/lib/validator";
 import Dropdown from "./Dropdown";
+import FileUploader from "./FileUploader";
+import { useState } from "react";
+import Image from "next/image";
 
 type EventFormProps = {
   userId: string;
@@ -32,6 +36,8 @@ type EventFormProps = {
 
 const initialFormValues = eventDefaultValues;
 const EventForm = ({ userId, type }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialFormValues,
@@ -49,18 +55,25 @@ const EventForm = ({ userId, type }: EventFormProps) => {
           className="flex flex-col gap-5"
         >
           <div className="flex flex-col gap-5 md:flex-row">
+            {/* Title */}
             <FormField
               control={form.control}
               name="title"
               render={({ field }) => (
-                <FormItem className="w-full">
+                <FormItem className="w-ful">
                   <FormControl>
-                    <Input placeholder="Event Title" {...field} />
+                    <Input
+                      placeholder="Event Title"
+                      className="input-field"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* Category */}
             <FormField
               control={form.control}
               name="categoryId"
@@ -77,6 +90,105 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               )}
             />
           </div>
+
+          <div className="flex flex-col gap-5 md:flex-row">
+            {/* Description */}
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl className="h-72">
+                    <Textarea
+                      placeholder="Description"
+                      className="textarea rounded-2xl"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Image */}
+            <FormField
+              control={form.control}
+              name="imageUrl"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl className="h-72">
+                    <FileUploader
+                      onFieldChange={field.onChange}
+                      imageUrl={field.value}
+                      setFiles={setFiles}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex flex-col gap-5 md:flex-row">
+            {/* Location */}
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl className="h-72">
+                    <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/location-grey.svg"
+                        alt="Calendar"
+                        width={24}
+                        height={24}
+                      />
+                      <Input
+                        placeholder="Location / Online"
+                        className="input-field"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex flex-col gap-5 md:flex-row">
+            {/* Date */}
+            <FormField
+              control={form.control}
+              name="startDateTime"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl className="h-72">
+                    <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/calendar.svg"
+                        alt="Calendar"
+                        width={24}
+                        height={24}
+                        className="filter-grey"
+                      />
+                      <p className="ml-3 whitespace-nowrap text-grey-600">
+                        Start Date:
+                      </p>
+                      {/* <Input
+                        placeholder="Location / Online"
+                        className="input-field"
+                        {...field}
+                      /> */}
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
           <Button type="submit">Submit</Button>
         </form>
       </Form>
